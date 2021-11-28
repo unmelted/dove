630d5a21e6123bbaecc9f6047f786963a4248177
eunkyung <unmelted@naver.com>
2021-11-28 20:58:30 +0900

    2DOF opticalflow test OK

--- main.cpp
+++ main.cpp
@@ -20,69 +20,73 @@ using namespace cv;
 
 
 const int HORIZONTAL_BORDER_CROP = 30;
+int MakeMask(Mat& mask, int width, int height);
 
-int main() {
-    string path = "image/";
-    vector<string>image_paths;
+int main(int argc, char* argv[]) {
 
-    namespace fs = std::__fs::filesystem;
+    char infile[40];
+    char outfile[40];
+    sprintf(infile,"%s.mp4", argv[1]);
+    sprintf(outfile, "%s_out1.mp4", argv[1]);
+    cout<<infile<<endl;
+    cout<<outfile<<endl;
 
-    for (const auto &entry : fs::directory_iterator(path)) {
-        if (fs::is_regular_file(entry) &&
-           (entry.path().extension().string() == ".png" ||
-            entry.path().extension().string() == ".jpg")) {
-            image_paths.push_back(entry.path().string());
-        }
-    }
+    VideoCapture stab(infile);
+    VideoWriter output;
+    output.open(outfile), VideoWriter::fourcc('M', 'J', 'P', 'G'), 30, Size(1920, 1080));    
 
-    sort(begin(image_paths), end(image_paths), less<string>()); 
-    vector<Mat> images;
-    for (const string &ip : image_paths) {        
-        cout << ip << endl;
-        images.push_back(imread(ip));
-    }
-
-    Mat src1; Mat src1o;
+    Mat src1; Mat src1oc; Mat src1o;
+    Mat mask;
     Mat src2;
+    Mat smth;
+
+    Mat pre_affine;
+    Mat affine;
     int cp_width = 0;
     int cp_height = 0;
     Rect srcrect;
     Rect dstrect;
+    char filename[30];
+    int scale = 2;
+    int i = 0;
+    int threshold = 6;
 
     TIMER* all;
     all = new TIMER();    
     StartTimer(all);    
+    smth.create(2 , 3 , CV_64F);    
+    MakeMask(mask, 1920/scale, 1080/scale);
 
-    for(int i = 0; i < images.size() ; i ++) {
+    while(true) {
 
-        src1o = images[i];
-        int owidth = int(src1o.cols/4);
-        resize(src1o, src1o, Size(src1o.cols/4, src1o.rows/4), 0,0,1);
+        stab >> src1oc;
+        if(src1oc.data == NULL)
+            break;
+
+        resize(src1oc, src1o, Size(int((float)src1oc.cols/scale), int(float(src1oc.rows)/scale)), 0,0,1);
         cvtColor(src1o, src1o, COLOR_BGR2GRAY);
 
         if( i == 0) {
             src2 = src1o;
+            sprintf(filename, "saved/%d_src1.png", i);
+            imwrite(filename, src1oc);
+            i++;
             continue;
         }
-        if(cp_width == 0 ) {
-            src1o.copyTo(src1);
-        }
-        else if(owidth != cp_width) {
-            src1 = src1o(dstrect);
-        } else {
-            src1o.copyTo(src1);            
-        }
 
-        cout<< " i : " <<i << " start " << endl; 
-        imwrite("saved/src1.png", src1);
-        imwrite("saved/src2.png", src2);
+        src1o.copyTo(src1);            
+        //cout<< " i : " <<i << " start " << endl; 
+        // sprintf(filename, "saved/%d_src1.png", i);        
+        // imwrite(filename, src1);
+        // sprintf(filename, "saved/%d_src2.png", i);        
+        // imwrite(filename, src2);
 
         vector <Point2f> features1, features2;
         vector <Point2f> goodFeatures1, goodFeatures2;
         vector <uchar> status;
         vector <float> err;
 
-        goodFeaturesToTrack(src1, features1, 100, 0.01  , 30 );
+        goodFeaturesToTrack(src1, features1, 30, 0.01  , 30, noArray(), 11, false, 0.04);
         calcOpticalFlowPyrLK(src1, src2, features1, features2, status, err );
 
         for(size_t i=0; i < status.size(); i++)
@@ -94,8 +98,16 @@ int main() {
             }
         }
 
-//        Mat affine = estimateRigidTransform(goodFeatures1, goodFeatures2, false);
-        Mat affine = estimateAffine2D(goodFeatures1, goodFeatures2);
+        if(goodFeatures1.size() < threshold || goodFeatures2.size() < threshold) {
+             cout<< i << " no feature to track.. feature cnt : "<< goodFeatures1.size() << endl;
+             sprintf(filename, "%d_no_feature.png", i);
+             imwrite(filename, src1oc);
+             pre_affine.copyTo(affine);
+         }
+         else {
+            //affine = estimateAffine2D(goodFeatures1, goodFeatures2);
+            affine = estimateRigidTransform(goodFeatures1, goodFeatures2, false);                
+        }
 
         double dx = affine.at<double>(0,2);
         double dy = affine.at<double>(1,2);
@@ -103,17 +115,27 @@ int main() {
         double ds_x = affine.at<double>(0,0)/cos(da);
         double ds_y = affine.at<double>(1,1)/cos(da);
 
-        cout << "dx : " << dx << "dy : "<< dy << endl;
+        smth.at<double>(0,0) = 1; //ds_x * cos(da);
+        smth.at<double>(0,1) = 0; //ds_x * -sin(da);
+        smth.at<double>(1,0) = 0; //ds_y * sin(da);
+        smth.at<double>(1,1) = 1; //ds_y * cos(da);
+        smth.at<double>(0,2) = dx;
+        smth.at<double>(1,2) = dy;
 
-        double realx = dx;
-        double realy = dy;
+        warpAffine(src1, src1, smth, src1.size());        
+        smth.at<double>(0,2) = dx * scale;
+        smth.at<double>(1,2) = dy * scale;      
+        warpAffine(src1oc, src1oc, smth, src1oc.size());
+/*
+        double realx = dx * scale;
+        double realy = dy * scale;
         double real_sx = ds_x;
         double real_sy = ds_y;
 
         Point ptsrc;
         Point ptdst;
-        cp_width = src1.cols - int(abs(realx));
-        cp_height = src1.rows - int(abs(realy));
+        cp_width = src1oc.cols - int(abs(realx));
+        cp_height = src1oc.rows - int(abs(realy));
 
         if(realx >= 0) {
             ptsrc.x = realx;
@@ -139,95 +161,30 @@ int main() {
         Mat transrc1;
         Mat transrc2;
         //src1(srcrect).copyTo(tranimg(dstrect));
-        src2(srcrect).copyTo(transrc2);
-        src1(dstrect).copyTo(transrc1);;
+        src1oc(dstrect).copyTo(transrc1);
+        src1(dstrect).copyTo(transrc2);
 
         char filename[30];
         sprintf(filename, "saved/%d_tran_src1.png", i);
         imwrite(filename, transrc1);
         sprintf(filename, "saved/%d_tran_src2.png", i);
         imwrite(filename, transrc2);
+*/
+        output << src1oc;
+        src1.copyTo(src2);     
+        affine.copyTo(pre_affine);   
 
-
-        transrc1.copyTo(src2);
+        //transrc1.copyTo(src2);
         
         Logger("[%d] %f ", i, LapTimer(all));
     }
 
 }
 
-/*
-int main()
-{
-
-    //Create a object of stabilization class
-    VideoStab stab;
-
-    //Initialize the VideoCapture object
-    VideoCapture cap("1.mp4");
-    //VideoCapture cap("videoplayback1.mp4");
-
-    Mat frame_2, frame2;
-    Mat frame_1, frame1;
-
-
-
-    cap >> frame_1;
-    cvtColor(frame_1, frame1, COLOR_BGR2GRAY);
-
-    Mat smoothedMat(2,3,CV_64F);
-
-    VideoWriter outputVideo;
-    outputVideo.open("1-1.mp4" , cv::VideoWriter::fourcc('X' , 'V' , 'I' , 'D'), 30 , frame_1.size());
-
-
-
-    //int i=0;
-    struct timeval time1{};
-    struct timeval time2{};
-
-    while(true)
-    {
-
-        cap >> frame_2;
-
-        if(frame_2.data == NULL)
-        {
-            break;
-        }
-
-        gettimeofday(&time1, nullptr);
-        cvtColor(frame_2, frame2, COLOR_BGR2GRAY);
-
-        Mat smoothedFrame;
-        smoothedFrame = stab.stabilize(frame_1 , frame_2);
-        outputVideo.write(smoothedFrame);
-        //i++;
-        //imwrite("file"+ std::to_string(i) +".jpg",smoothedFrame);
-        //imwrite("file"+ std::to_string(i) + std::to_string(i) +".jpg",frame_2);
-        gettimeofday(&time2, nullptr);
-        time_t msec_time = (time2.tv_sec - time1.tv_sec)*1000 + (time2.tv_usec - time1.tv_usec) /1000;
-        cout << "msec : " << msec_time << endl;
-
-
-        //imshow("Stabilized Video" , smoothedFrame);
-
-
-
-
-
-
-        char c = (char) waitKey(10);
-            if( c == 27 )
-              break;
-
-
-        frame_1 = frame_2.clone();
-        frame2.copyTo(frame1);
-
-
-    }
-
-    return 0;
+int MakeMask(Mat& mask, int width, int height) {
+    cout<< "mask width , height : " << width << " , "<< height<<endl;
+    int border = width/12;
+    mask = Mat::zeros(height, width, CV_8UC1);
+    rectangle(mask, Point(631, 247), Size(300, 200), Scalar(255), -1);
+    imwrite("mask.png", mask);
 }
-*/
