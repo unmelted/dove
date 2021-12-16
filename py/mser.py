2aa4aac5f31d4b08178eae69f2e128aad60fb930
eunkyung <unmelted@naver.com>
2021-12-17 02:17:14 +0900

    1217 midnight

--- py/mser.py
+++ py/mser.py
@@ -100,43 +100,86 @@ class dt() :
     peak_summ = 0
     isSwipe = False
     normal = 0;
-    bgc = None
     bgcg = None
+    skip = 100
 
     def detectsequence(self, bg, cur, index) :
         #cv2.imwrite("{}_cur.png".format(index), cur)    
         cur = cv2.resize(cur, (640, 480))
         cur = cv2.GaussianBlur(cur, (3, 3), 0)
         diff = None
-        if self.isSwipe == True :
-            diff = cv2.subtract(self.bgcg, cur)
-        else : 
-            diff = cv2.subtract(bg, cur)        
+        # if self.isSwipe == True : 
+        #     diff = cv2.subtract(self.bgcg, cur)                    
+        # else : 
+        diff = cv2.subtract(bg, cur)        
 
         self.summ = cv2.sumElems(diff)[0] / (640* 480)
         
+        # if self.isSwipe == True and self.skip > 70 :
+        #     self.summ = 10
+        #     self.skip -= 1
+        #     self.bgcg = cur.copy()
+
         if index == 1 :
             self.first_summ = self.summ
         else :
             if self.isSwipe == False :
                 if self.summ - self.prev_summ > self.first_summ * self.threshold :
                     self.isSwipe = True
-                    self.bgcg = cur.copy()
+                    #self.bgcg = cur.copy()                    
                 else :
                     self.isSwipe = False
 
             elif self.isSwipe == True :
-                if self.summ <= self.first_summ : 
+                if self.summ <= 2: 
                     self.isSwipe = False
                 else :
                     self.isSwipe = True
-                self.bgcg = cur.copy()
 
         self.prev_diff = self.summ - self.prev_summ
         self.prev_summ = self.summ
         return diff, self.summ, self.isSwipe
 
-cap = cv2.VideoCapture(datapath+'movie/4dmaker_600.mp4')
+class KalmanFilter() :
+    F = None
+    B = None
+    H = None
+    Q = None
+    R = None
+    P = None
+    x0 = None
+    def init(self, F, H, Q , R) :
+        if(F is None or H is None):
+            raise ValueError("Set proper system dynamics.")
+
+        self.n = F.shape[1]
+        self.m = H.shape[1]
+
+        self.F = F
+        self.H = H
+        self.B = 0
+        self.Q = np.eye(self.n) if Q is None else Q
+        self.R = np.eye(self.n) if R is None else R
+        self.P = np.eye(self.n)
+        self.x = np.zeros((self.n, 1))
+
+    def predict(self, u = 0):
+        self.x = np.dot(self.F, self.x) + np.dot(self.B, u)
+        self.P = np.dot(np.dot(self.F, self.P), self.F.T) + self.Q
+        return self.x
+
+    def update(self, z):
+        y = z - np.dot(self.H, self.x)
+        S = self.R + np.dot(self.H, np.dot(self.P, self.H.T))
+        K = np.dot(np.dot(self.P, self.H.T), np.linalg.inv(S))
+        self.x = self.x + np.dot(K, y)
+        I = np.eye(self.n)
+        self.P = np.dot(np.dot(I - np.dot(K, self.H), self.P), 
+        (I - np.dot(K, self.H)).T) + np.dot(np.dot(K, self.R), K.T)
+
+
+out = cv2.VideoWriter('test_out.avi', cv2.VideoWriter_fourcc(*"MJPG"), 30,(1920, 1080))
+cap = cv2.VideoCapture(datapath+'movie/4dmaker_626.mp4')
 index = 1
 ret, bg = cap.read()
 prevc = bg.copy()
@@ -158,6 +201,17 @@ ptrack_ceny = 0
 mask = 0
 curc = None
 ismove = False
+warp_dst = None
+
+dt = 1.0/60
+F = np.array([[1, dt, 0], [0, 1, dt], [0, 0, 1]])
+H = np.array([1, 0, 0]).reshape(1, 3)
+Q = np.array([[0.0001, 0.0001, 0.0], [0.0001, 0.0001, 0.0], [0.0, 0.0, 0.0]])
+R = np.array([0.2]).reshape(1, 1)
+kfx = KalmanFilter()
+kfx.init(F, H, Q, R)
+kfy = KalmanFilter()
+kfy.init(F, H, Q, R)
 
 while(cap.isOpened()):
     print(" -- ", index)
@@ -169,14 +223,14 @@ while(cap.isOpened()):
     img = cv2.resize(img, (1920, 1080))
     curc = img
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    #cv2.imwrite("{}_cur.png".format(index), gray)            
+    cv2.imwrite("saved/{}_cur.png".format(index), curc)            
     cur = gray.copy()
     #cv2.imwrite("{}_cur.png".format(index), cur)        
     diff, noise, ismove = ddd.detectsequence(bg_gray, cur, index)
-    if ismove == True :
-        print(":::::::: ------------ swipe ------------- :::::::::::", noise)
-    else : 
-        print("------------ no -------------", noise)
+    # if ismove == True :
+    #     print(":::::::: ------------ swipe ------------- :::::::::::", noise)
+    # else : 
+    #     print("------------ no -------------", noise)
 
     dst = diff
     mser = cv2.MSER_create(5, 170, 16000, 0.5)
@@ -215,6 +269,7 @@ while(cap.isOpened()):
     #print("before confirm.. ")
     print(confirm)
     len_con = len(confirm)
+    ffound = False    
     if len_con > 0 and found == False and confirm[len_con -1][2] * confirm[len_con -1][2] > 200:
         found = True
         track_obj = confirm[len_con -1]
@@ -224,7 +279,6 @@ while(cap.isOpened()):
         print(" -------- track start ", track_obj, track_cenx, track_ceny)
 
     elif len_con > 0 and found == True :
-        ffound = False
         for ii, i in enumerate(confirm) :
             #if abs(i[0] - track_obj[0]) < 10 and abs(i[1] - track_obj[1]) < 10 and abs(i[2] - track_obj[2]) < 15 and abs(i[3] - track_obj[3]) < 15 :
             icen_x = i[0] + i[2] /2
@@ -253,20 +307,21 @@ while(cap.isOpened()):
             cv2.rectangle(clone, (c[0], c[1]), (c[0]+c[2],c[1]+c[3]), (255, 0, 255), 2)            
             cv2.putText(clone, "FOCUS!", (c[0], c[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.4,  (255, 255, 40), 1)
             cv2.rectangle(clone, (roi_sx, roi_sy), (roi_sx + roi_w, roi_sy + roi_h), (255), 2)
+            cv2.imwrite("black/{}_clone.png".format(index), clone)            
         di += 1
 
-    if found == True :
-        if ismove == True:
-            cv2.imwrite("{}_prev.png".format(index), prev)
+    if ffound == True :
+        if ismove == 3:
+            #cv2.imwrite("{}_prev.png".format(index), prev)
             cur_crop = cur[roi_sy * 3 : (roi_sy + roi_h) * 3, roi_sx * 3 : (roi_sx + roi_w) * 3]
             prev_crop = prev[roi_sy * 3 : (roi_sy + roi_h) * 3, roi_sx * 3 : (roi_sx + roi_w) * 3]
-            cv2.imwrite("{}_curcrop.png".format(index), cur_crop)
-            cv2.imwrite("{}_prevcrop.png".format(index), prev_crop)        
-            pt1 = cv2.goodFeaturesToTrack(prev_crop, 100, 0.02, 2)
+            cv2.imwrite("{}_curcrop1.png".format(index), cur_crop)
+            cv2.imwrite("{}_prevcrop1.png".format(index), prev_crop)        
+            pt1 = cv2.goodFeaturesToTrack(prev_crop, 200, 0.01, 10)
             pt2, status, err = cv2.calcOpticalFlowPyrLK(prev_crop, cur_crop, pt1, None)
             gdpt1 = []
             gdpt2 = []
-            for i in range(0, len(pt2)) :
+            for i in range(pt2.shape[0]) :
                 gdpt1.append(pt1[i])
                 gdpt2.append(pt2[i])
             
@@ -277,36 +332,70 @@ while(cap.isOpened()):
             warp_m[1][1] = 1  
             warp_m[0][2] = warp_m[0][2]
             warp_m[1][2] = warp_m[1][2]
-            print("warp matrix  ", warp_m[0][2], warp_m[1][2])
+            print("warp matrix  1", warp_m[0][2], warp_m[1][2])
             warp_dst = cv2.warpAffine(prevc, warp_m, (1920,1080))
         else : 
-            cv2.imwrite("{}_prev.png".format(index), prev)            
-            cur_crop = cur[roi_sy * 3 : (roi_sy + roi_h) * 3, roi_sx * 3 : (roi_sx + roi_w) * 3]
-            prev_crop = prev[roi_sy * 3 : (roi_sy + roi_h) * 3, roi_sx * 3 : (roi_sx + roi_w) * 3]
-            cv2.imwrite("{}_curcrop.png".format(index), cur_crop)
-            cv2.imwrite("{}_prevcrop.png".format(index), prev_crop)        
-            pt1 = cv2.goodFeaturesToTrack(prev_crop, 100, 0.02, 2)
-            pt2, status, err = cv2.calcOpticalFlowPyrLK(prev_crop, cur_crop, pt1, None)
+            #cv2.imwrite("{}_prev.png".format(index), prev)            
+            #cur_crop = cur[roi_sy * 3 : (roi_sy + roi_h) * 3, roi_sx * 3 : (roi_sx + roi_w) * 3]
+            #prev_crop = prev[roi_sy * 3 : (roi_sy + roi_h) * 3, roi_sx * 3 : (roi_sx + roi_w) * 3]
+            #cv2.imwrite("{}_curcrop2.png".format(index), cur_crop)
+            #cv2.imwrite("{}_prevcrop2.png".format(index), prev_crop)        
+            pt1 = cv2.goodFeaturesToTrack(prev, 200, 0.01, 10)
+            pt2, status, err = cv2.calcOpticalFlowPyrLK(prev, cur, pt1, None)
             gdpt1 = []
             gdpt2 = []
-            for i in range(0, len(pt2)) :
+            for i in range(pt2.shape[0]) :
                 gdpt1.append(pt1[i])
                 gdpt2.append(pt2[i])
             
             warp_m, _ = cv2.estimateAffine2D(np.array(gdpt1).astype(np.float32), np.array(gdpt2).astype(np.float32))
+            kfx.update((track_cenx - ptrack_cenx) *3)
+            kfy.update((track_ceny - ptrack_ceny) *3)                 
+            px = (np.dot(H,  kfx.predict())[0])
+            py = (np.dot(H,  kfy.predict())[0])
             warp_m[0][0] = 1
             warp_m[0][1] = 0
             warp_m[1][0] = 0        
             warp_m[1][1] = 1  
-            warp_m[0][2] = warp_m[0][2]
-            warp_m[1][2] = warp_m[1][2]
-            print("warp matrix  ", warp_m[0][2], warp_m[1][2])
+            warp_m[0][2] = px * 0.3 #track_cenx - ptrack_cenx#warp_m[0][2]
+            warp_m[1][2] = py * 0.3#track_ceny - ptrack_ceny#warp_m[1][2]
+            print("warp matrix 2 ", warp_m[0][2], warp_m[1][2])                        
+            cv2.imshow("TEST", warp_dst)
+
+            if track_cenx - ptrack_cenx == 0:
+                px = (np.dot(H,  kfx.predict())[0])
+                py = (np.dot(H,  kfy.predict())[0])
+                warp_m[0][0] = 1
+                warp_m[0][1] = 0
+                warp_m[1][0] = 0        
+                warp_m[1][1] = 1  
+                warp_m[0][2] = px * 0.3 #track_cenx - ptrack_cenx#warp_m[0][2]
+                warp_m[1][2] = py * 0.3#track_ceny - ptrack_ceny#warp_m[1][2]            
+            #kfx.update(warp_m[0][2])
+            #kfy.update(warp_m[1][2])
+            #curc_crop = curc[roi_sy * 3 : (roi_sy + roi_h) * 3, roi_sx * 3 : (roi_sx + roi_w) * 3]            
             warp_dst = cv2.warpAffine(prevc, warp_m, (1920,1080))
+            #cur = cv2.warpAffine(cur, warp_m, (1920,1080))            
+            cv2.imshow("TEST", warp_dst)
+            
+            px = (np.dot(H,  kfx.predict())[0])
+            py = (np.dot(H,  kfy.predict())[0])
+            warp_m[0][0] = 1
+            warp_m[0][1] = 0
+            warp_m[1][0] = 0        
+            warp_m[1][1] = 1  
+            warp_m[0][2] = px * 0.3#track_cenx - ptrack_cenx#warp_m[0][2]
+            warp_m[1][2] = py * 0.3#track_ceny - ptrack_ceny#warp_m[1][2]            
+            #kfx.update(warp_m[0][2])
+            #kfy.update(warp_m[1][2])
+            #curc_crop = curc[roi_sy * 3 : (roi_sy + roi_h) * 3, roi_sx * 3 : (roi_sx + roi_w) * 3]            
+            warp_dst = cv2.warpAffine(prevc, warp_m, (1920,1080))            
     else :
         warp_dst = prevc.copy() 
 
     terminate_time = timeit.default_timer()
     print("TIME : %f s" % (terminate_time - start_time))
+    out.write(warp_dst)
 
     cv2.imwrite("{}_warp.png".format(index), warp_dst)
     prevc = curc.copy()
@@ -318,7 +407,7 @@ while(cap.isOpened()):
     ptrack_cenx = track_cenx
     ptrack_ceny = track_ceny
 
-    cv2.imshow("TEST", clone)
+    cv2.imshow("TEST", warp_dst)
     #cv2.waitKey()
     if cv2.waitKey(25) & 0xFF == ord('q') :
         break
